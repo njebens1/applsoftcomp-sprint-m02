4bdfdf2be652c78cd5d5f661c75fbcf33f3ff63b
Nick Jebens <njebens1@binghamton.edu>
2026-03-09 15:44:26 -0400

    Add workflow script

---
+++ run.sh
@@ -0,0 +1,10 @@
+python workflow/format_1d_data.py
+python workflow/viz_1d_data.py
+
+python workflow/format_2d_data.py
+python workflow/viz_2d_data.py
+
+python workflow/format_multi_method.py
+python workflow/viz_multi_method.py
+
+python workflow/viz_digits.py
